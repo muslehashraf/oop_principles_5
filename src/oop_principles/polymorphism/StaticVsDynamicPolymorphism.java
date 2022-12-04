fa986f6048e1d39e44a2006a194ee0e7589a028d
Ashraf Musleh <muslehashraf@gmail.com>
2022-12-04 14:46:15 -0600

    ..

--- src/oop_principles/polymorphism/StaticVsDynamicPolymorphism.java
+++ src/oop_principles/polymorphism/StaticVsDynamicPolymorphism.java
@@ -1,4 +1,16 @@
 package oop_principles.polymorphism;
 
 public class StaticVsDynamicPolymorphism {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        animal.eat(); // static polymorphism
+
+        Animal dog = new Dog(); // up-casted dog object
+        dog.sleep(); // dynamic polymorphism
+        // as compiler thinks the Animal method will be executed
+        // Runtime finds out that the Dog method will be executed
+
+    }
+
 }
